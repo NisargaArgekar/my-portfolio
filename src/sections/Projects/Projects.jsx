15344bc8e7936bb05058afd84c4e8f82bb836236
Nisarga <nisargaargekar2002@gmail.com>
2025-05-26 23:12:51 +0530

    updates

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from './ProjectsStyles.module.css';
-import foodwebsite from '../../assets/fresh-burger.png'
 import freshBurger from '../../assets/fresh-burger.png';
-import dictionary from  '../../assets/dictionary.png'
+import dictionary from  '../../assets/dictionary.png';
+import icon from '../../assets/icon.png';
 import fooddelivery from '../../assets/fooddelivery.jpg'
 import ProjectCard from '../../common/ProjectCard';
 
@@ -11,6 +11,12 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
+          src={icon}
+          link="https://hire-vista.vercel.app"
+          h3="HireVista"
+          p="AI-Enhanced Job Portal"
+        />
+        <ProjectCard
           src={fooddelivery}
           link="https://bite-meal.vercel.app"
           h3="Bite Meal"
@@ -19,8 +25,8 @@ function Projects() {
         <ProjectCard
           src={dictionary}
           link="https://diction-snap.vercel.app"
-          h3="AI-Generated Word Searcher"
-          p="Diction-Snap"
+          h3="Diction-Snap"
+          p="AI-Generated Word Searcher"
         />
         <ProjectCard
           src={freshBurger}

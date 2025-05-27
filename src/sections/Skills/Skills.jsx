b29a5dfc463e83ad99926277a4ffda3245fb19e2
Nisarga <nisargaargekar2002@gmail.com>
2025-05-27 14:13:40 +0530

    skill

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -15,7 +15,9 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Firebase" />
+        
       </div>
       <hr />
       <div className={styles.skillList}>
